fd57d2bb7b3aaa580c3e283ada7a6e62a4f21051
maxwondercorn <gmartell@pobox.com>
2024-05-22 22:19:42 -0400

    chore: Misc fixes/improvements

--- SQL/area lookup.sql
+++ SQL/area lookup.sql
@@ -1,7 +1,8 @@
 SELECT 
 area.id,
-area.name as area,
-value_stream.name as `value strem`
+value_stream.name as VS,
+area.name as area
 FROM area
 JOIN value_stream ON area.value_stream_id = value_stream.id
 WHERE area.location_id = 3
+ORDER BY VS
