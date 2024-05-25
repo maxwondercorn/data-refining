4c8df035aa9a790fba2a93376ca43d9c2e64876b
maxwondercorn <gmartell@pobox.com>
2024-05-25 00:32:52 -0400

    feat: Added location code

--- SQL/area lookup.sql
+++ SQL/area lookup.sql
@@ -1,8 +1,10 @@
-SELECT 
+SELECT
 area.id,
+location.code,
 value_stream.name as VS,
 area.name as area
 FROM area
 JOIN value_stream ON area.value_stream_id = value_stream.id
+JOIN location ON area.location_id = location.id
 WHERE area.location_id = 3
 ORDER BY VS
